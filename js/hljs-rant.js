ae36440504e6b6a5796366068b553f13719ad170
Nicholas Fleck <TheBerkin@users.noreply.github.com>
2020-09-28 10:43:33 -0500

    A bunch of pages are now finished, but it's not done

---
+++ js/hljs-rant.js
@@ -0,0 +1,70 @@
+let rant_def = {
+    contains: [
+        hljs.COMMENT(
+          '##',
+          '##'
+        ),
+        hljs.COMMENT(
+          '#',
+          '$'
+        ),
+        {
+          // String literal
+          className: 'string',
+          begin: /"/,
+          end: /"/
+        },
+        {
+          // Number
+          className: 'number',
+          begin: /\b\d+(\.\d+)?/,
+        },
+        {
+          // Empty
+          className: 'keyword',
+          begin: /\<\>/
+        },
+        {
+          // Escape sequence
+          className: 'string',
+          begin: /\\(x[a-fA-F0-9]{2}|\S)/
+        },
+        {
+          // Closure
+          className: 'keyword',
+          begin: /\[\?\:?/,
+          end: /\]/,
+          contains: [{
+            begin: /[\w-_]+/,
+            end: /\;?/,
+          }]
+        },
+        {
+          // Map operator
+          className: 'keyword',
+          begin: /\@/
+        },
+        {
+          // Function definition
+          className: 'variable',
+          begin: /(?<=\[)\$[\w-_]+/g
+        },
+        {
+          // Function call
+          className: 'title',
+          begin: /(?<=\[)([\w-_]+|\!)/g
+        },
+        {
+          // Variable accessor start
+          className: 'variable',
+          begin: /\<\$?/
+        },
+        {
+          // Variable accessor end
+          className: 'variable',
+          begin: /\>/
+        }
+    ]
+  };
+  
+  hljs.registerLanguage('rant', () => rant_def);
